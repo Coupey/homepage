ddd2ad3377dd6d29192d931959fb0428b6cb521a
Paul Coupe <paul@coupey.co.uk>
2018-12-20 14:34:25 +0000

    ch ch ch ch changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # homepage
-My default start page
+My default start page. This is ancient ancient ancient (jQuery! Backbone! Comical CSS), but I just haven't had time or inclination to update it. I might one day.
--- js/search.js
+++ js/search.js
@@ -1,110 +1,93 @@
+function doAmazonSearch() {
+  searchTerm = document.getElementById("textfield1").value;
+  //alert(searchTerm);
 
-    function doAmazonSearch()
-    {
-    searchTerm = document.getElementById("textfield1").value;
-    //alert(searchTerm);
+  searchStem = "https://www.amazon.co.uk/s?ie=UTF8&index=blended&keywords=" + searchTerm;
+  document.location.href = searchStem;
+}
 
-    searchStem = "http://www.amazon.co.uk/exec/obidos/external-search/202-1958194-9555020?tag=opera-20&index=blended&keyword=" + searchTerm
-    document.location.href = searchStem;
+function doEbaySearch() {
+  searchTerm = document.getElementById("textfield2").value;
+  searchStem = "https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=" + searchTerm
+  document.location.href = searchStem;
 
-    }
+}
 
-    function doEbaySearch()
-    {
-    searchTerm = document.getElementById("textfield2").value;
-    searchStem = "http://search.ebay.co.uk/search/search.dll?MfcISAPICommand=GetResult&ht=1&SortProperty=MetaEndSort&cgiurl=http%3A%2F%2Fcgi.ebay.co.uk%2Fws%2F&query=" + searchTerm
-    document.location.href = searchStem;
+function doYouTubeSearch() {
+  searchTerm = document.getElementById("textfield5").value;
+  searchStem = "https://www.youtube.com/results?search_query=" + searchTerm + "&&search=Search";
+  document.location.href = searchStem;
+}
 
-    }
+function doWikiSearch() {
+  searchTerm = document.getElementById("textfield6").value;
+  searchStem = "https://en.wikipedia.org/w/index.php?search=" + searchTerm + "&title=Special%3ASearch&go=Go";
+  document.location.href = searchStem;
 
-    function doYouTubeSearch()
-    {
-    searchTerm = document.getElementById("textfield5").value;
-    searchStem = "http://www.youtube.com/results?search_query=" + searchTerm + "&&search=Search";
-    document.location.href = searchStem;
+}
 
-    }
-    function doWikiSearch()
-    {
-    searchTerm = document.getElementById("textfield6").value;
-    searchStem = "http://en.wikipedia.org/wiki/Special:Search/" + searchTerm;
-    document.location.href = searchStem;
+function doGmapSearch() {
+  searchTerm = document.getElementById("textfield7").value;
+  searchStem = "https://maps.google.co.uk/maps?f=q&hl=en&q=" + searchTerm;
+  document.location.href = searchStem;
 
-    }
-    function doGmapSearch()
-    {
-    searchTerm = document.getElementById("textfield7").value;
-    searchStem = "http://maps.google.co.uk/maps?f=q&hl=en&q=" + searchTerm;
-    document.location.href = searchStem;
-
-    }
+}
 
 
+function doGoogleSearch() {
+  searchTerm = document.getElementById("textfield3").value;
+  //alert(searchTerm);
 
-    function doGoogleSearch()
-    {
-    searchTerm = document.getElementById("textfield3").value;
-    //alert(searchTerm);
+  searchStem = "http://www.google.co.uk/search?q=" + searchTerm + "&ie=ISO-8859-1&hl=en&btnG=Google+Search";
+  document.location.href = searchStem;
 
-    searchStem = "http://www.google.co.uk/search?q=" + searchTerm + "&ie=ISO-8859-1&hl=en&btnG=Google+Search";
-    document.location.href = searchStem;
+}
 
-    }
 
-    function doBinarySearch()
-    {
-    searchTerm = document.getElementById("textfield4").value;
-    //alert(searchTerm);
+function showCalendar(name) {
+  if (name == "Paul") {
+    //alert("show paul");
+    document.getElementById("calendar_di").style.display = "none";
+    document.getElementById("calendar_paul").style.display = "block";
+    createCookie("calendar", "paul")
+  } else {
+    //alert("show di");
+    document.getElementById("calendar_di").style.display = "block";
+    document.getElementById("calendar_paul").style.display = "none";
+    createCookie("calendar", "di")
+  }
+}
 
-    searchStem = "http://alt.binaries.nl/search/?q=" + searchTerm
-    document.location.href = searchStem;
+function createCookie(name, value, days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    var expires = "; expires=" + date.toGMTString();
+  }
+  else var expires = "";
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
 
-    }
+function readCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
 
-    function showCalendar(name){
-        if (name == "Paul"){
-                //alert("show paul");
-                document.getElementById("calendar_di").style.display = "none";
-                document.getElementById("calendar_paul").style.display = "block";
-                createCookie("calendar","paul")
-        } else {
-                //alert("show di");
-                document.getElementById("calendar_di").style.display = "block";
-                document.getElementById("calendar_paul").style.display = "none";
-                createCookie("calendar","di")
-        }
-    }
+function eraseCookie(name) {
+  createCookie(name, "", -1);
+}
 
-    function createCookie(name,value,days) {
-        if (days) {
-                var date = new Date();
-                date.setTime(date.getTime()+(days*24*60*60*1000));
-                var expires = "; expires="+date.toGMTString();
-        }
-        else var expires = "";
-        document.cookie = name+"="+value+expires+"; path=/";
-    }
-
-    function readCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-                var c = ca[i];
-                while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
-    }
-
-    function eraseCookie(name) {
-        createCookie(name,"",-1);
-    }
-
-    function showSavedCalendar(){
-        calVar = readCookie("calendar");
-        if (calVar == "di"){
-                showCalendar("Di");
-        } else {
-                showCalendar("Paul");
-        }
-    }
+function showSavedCalendar() {
+  calVar = readCookie("calendar");
+  if (calVar == "di") {
+    showCalendar("Di");
+  } else {
+    showCalendar("Paul");
+  }
+}
